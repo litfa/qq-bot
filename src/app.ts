173c636f0c192efe2adedd4224ceb91bb631dbb4
Litfa <admin@litf.com.cn>
2022-07-24 16:06:22 +0800

    feat: 基础框架

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import insertSql from './service/insertSql'
 import { ws } from './utils/ws'
 import cron from './service/cron'
+import { readdirSync } from 'fs'
 
 ws.on('open', () => {
   cron()
   insertSql()
 })
+
+const plugins = readdirSync('./plugins')
+
+plugins
+  .filter((e) => /^./.test(e))
+  .forEach((e) => {
+    require(`../plugins/${e}`).main()
+  })
+
+console.log(plugins)
--- src/utils/ws.ts
+++ src/utils/ws.ts
@@ -5,12 +5,19 @@ const ws = new WebSocket(
   `ws://${config.bot.host}:${config.bot.port}/all?verifyKey=${config.bot.verifyKey}&qq=${config.bot.qq}`
 )
 
-const send = (e: any) => {
-  if (typeof e == 'object') {
-    return ws.send(JSON.stringify(e))
+const send = (json: any) => {
+  if (typeof json == 'object') {
+    return ws.send(JSON.stringify(json))
   } else {
-    return ws.send(e.toString())
+    return ws.send(json.toString())
   }
 }
 
-export { send, ws }
+const message = (data: (data: JSON) => void) => {
+  ws.on('message', (e) => {
+    // @ts-ignore
+    data(JSON.parse(e))
+  })
+}
+
+export { send, ws, message }
