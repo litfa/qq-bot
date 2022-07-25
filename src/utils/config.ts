3ef6f6ed5bd228c2c9da53f2b8b61f64154a92d5
Litfa <admin@litf.com.cn>
2022-07-25 21:21:37 +0800

    fix: 配置文件

---
+++ src/utils/config.ts
@@ -0,0 +1,31 @@
+import { readFileSync } from 'fs'
+import { parse } from 'yaml'
+
+export default <
+  {
+    port: number
+    bot: {
+      host: string
+      port: number
+      verifyKey: string
+      qq: number
+    }
+    mysql: {
+      'host': string
+      'port': number
+      'user': string
+      'password': string
+      'database': string
+    }
+    cron: {
+      time: string
+      groups: number[]
+      friends: number[]
+      template: string
+    }[]
+    sendNudge: {
+      command: string
+      command_sender: number
+    }
+  }
+>parse(readFileSync('config.yml', 'utf8'))
