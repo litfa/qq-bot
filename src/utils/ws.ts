4a7a524da362102860a821937d796f3a8f6a5177
ltfei <ltfei@ltfei.cn>
2023-02-24 18:40:30 +0800

    fix: 包路径

--- src/utils/ws.ts
+++ src/utils/ws.ts
@@ -1,30 +1,6 @@
-// import WebSocket from 'ws'
+
 import config from './config'
-// import type data from '../types/data'
-
-// const ws = new WebSocket(
-//   `ws://${config.bot.host}:${config.bot.port}/all?verifyKey=${config.bot.verifyKey}&qq=${config.bot.qq}`
-// )
-
-// const send = (json: any) => {
-//   if (typeof json == 'object') {
-//     return ws.send(JSON.stringify(json))
-//   } else {
-//     return ws.send(json.toString())
-//   }
-// }
-
-// const message = (data: (data: data) => void) => {
-//   ws.on('message', (e) => {
-//     // @ts-ignore
-//     data(JSON.parse(e))
-//   })
-// }
-
-// export { send, ws, message }
-
-// import { CreateMiraiApi } from 'typescript-mirai-api-http'
-import { CreateMiraiApi } from '../../../mirai-api-http-ts'
+import { CreateMiraiApi } from 'typescript-mirai-api-http'
 
 const { onMessage, send, open, error, close } = CreateMiraiApi(
   config.bot.host,
