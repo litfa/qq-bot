b4e28c18403c23135310d558769d6490132de9ab
ltfei <ltfei@ltfei.cn>
2023-04-21 20:16:16 +0800

    feat: 好友消息查询

--- src/plugins/webqq/router/getMessageRecord.ts
+++ src/plugins/webqq/router/getMessageRecord.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express'
 import { query } from '../../../utils/db'
 import { sqlToObj } from '../../sql_v2/utils'
-import { GroupMessageType, GroupSyncMessageType } from '../types/Message'
+import { FriendMessageType, FriendSyncMessageType } from '../types/Message'
 
 const router = Router()
 
-const converResult = <T extends GroupMessageType | GroupSyncMessageType>(
+const converResult = <T extends FriendMessageType | FriendSyncMessageType>(
   data: T
 ): T => {
   const res = sqlToObj(data) as T
@@ -15,11 +15,48 @@ const converResult = <T extends GroupMessageType | GroupSyncMessageType>(
   return res
 }
 
-router.post('/friend')
+router.post('/friend', async (req, res) => {
+  const { qq, lastId = 2147483647, limit = 100 } = req.body
+  let err, result
+  ;[err, result] = await query<FriendMessageType[]>(
+    'select * from friend_message where id<? and sender__id=? order by timestamp desc limit ?',
+    [lastId, qq, limit]
+  )
+  if (err) {
+    return res.send({
+      status: 500
+    })
+  }
+  const friendMessage = result.map(converResult)
+  const minTimestamp = friendMessage.at(-1)?.timestamp
+  const maxTimestamp = friendMessage[0]?.timestamp
+
+  ;[err, result] = await query(
+    'select * from friend_sync_message where timestamp>? and timestamp<? and subject__id=? order by timestamp desc limit ?;    ',
+    [minTimestamp, maxTimestamp, qq, limit]
+  )
+
+  if (err) {
+    return res.send({
+      status: 500
+    })
+  }
+
+  const syncMessage = result.map(converResult) as FriendSyncMessageType[]
+
+  const message = [...friendMessage, ...syncMessage].sort((a, b) => {
+    return a.timestamp - b.timestamp
+  })
+
+  res.send({
+    status: 200,
+    data: message
+  })
+})
 router.post('/group', async (req, res) => {
   const { group, lastId = 2147483647, limit = 100 } = req.body
   let err, result
-  ;[err, result] = await query<GroupMessageType[]>(
+  ;[err, result] = await query<FriendMessageType[]>(
     'select * from group_message where id<? and sender__group__id=? order by timestamp desc limit ?',
     [lastId, group, limit]
   )
@@ -30,7 +67,7 @@ router.post('/group', async (req, res) => {
   }
   const groupMessage = result.map(converResult)
   const minTimestamp = groupMessage.at(-1)?.timestamp
-  const maxTimestamp = groupMessage[0].timestamp
+  const maxTimestamp = groupMessage[0]?.timestamp
 
   ;[err, result] = await query(
     'select * from group_sync_message where timestamp>? and timestamp<? and subject__id=? order by timestamp desc limit ?;    ',
@@ -43,7 +80,7 @@ router.post('/group', async (req, res) => {
     })
   }
 
-  const syncMessage = result.map(converResult) as GroupSyncMessageType[]
+  const syncMessage = result.map(converResult) as FriendSyncMessageType[]
 
   const message = [...groupMessage, ...syncMessage].sort((a, b) => {
     return a.timestamp - b.timestamp
