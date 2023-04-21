28f468a600e9794d540dc271a45aa9f88ae7575f
ltfei <ltfei@ltfei.cn>
2023-04-21 16:58:26 +0800

    feat: 优化格式

--- src/plugins/sql-v2/insertSql.ts
+++ src/plugins/sql-v2/insertSql.ts
@@ -51,28 +51,28 @@ export const insertSql = (
     'TempMessage',
     'TempSyncMessage'
   ]
-  if (types.includes(data.data?.type)) {
-    const date = transferConfig?.date || new Date()
-    const uuid = uuidV4()
-    const tosql = toSqlData(data.data, {
-      date,
-      timestamp: date.getTime(),
-      uuid
-    })
-
-    // data.data.type 已经判断过，是39种类型，直接拼接是安全的
-    const data1 = query(
-      `insert into ${toUnderscoreCase(data.data.type)} set ?`,
-      [tosql]
-    )
-    // 原格式
-    const data2 = query('insert into message_text set ?', {
-      date,
-      timestamp: date.getTime(),
-      uuid,
-      text: JSON.stringify(data)
-    })
-
-    return [data1, data2]
+  if (!types.includes(data.data?.type)) {
+    return
   }
+  const date = transferConfig?.date || new Date()
+  const uuid = uuidV4()
+  const tosql = toSqlData(data.data, {
+    date,
+    timestamp: date.getTime(),
+    uuid
+  })
+
+  // data.data.type 已经判断过，是39种类型，直接拼接是安全的
+  const data1 = query(`insert into ${toUnderscoreCase(data.data.type)} set ?`, [
+    tosql
+  ])
+  // 原格式
+  const data2 = query('insert into message_text set ?', {
+    date,
+    timestamp: date.getTime(),
+    uuid,
+    text: JSON.stringify(data)
+  })
+
+  return [data1, data2]
 }
