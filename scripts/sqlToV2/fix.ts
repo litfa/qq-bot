dff0264f7e7f55cff3ab572f44bf1977ca74ca8c
ltfei <ltfei@ltfei.cn>
2023-06-13 23:29:02 +0800

    fix: 移除升级脚本

--- scripts/sqlToV2/fix.ts
+++
@@ -1,9 +0,0 @@
-import { query } from '../../src/utils/db'
-
-export default async () => {
-  const [err, result] = await query(`
-  alter table friend_recall_event
-      modify operator bigint null;
-  `)
-  console.log('执行完成', err, result)
-}
