10c300b8080395dc0a92460aeee8128b318ab8f8
alongw <alongw@alongw.cn>
2023-06-13 21:53:29 +0800

    feat: 完善hello world插件

--- src/plugins/hello_world/index.ts
+++ src/plugins/hello_world/index.ts
@@ -1,16 +1,20 @@
 import { onMessage, sendFriendMessage } from '../../utils/ws'
+import config from './../../utils/config'
 
 export const Main = () => {
   onMessage((data) => {
     if (
       data?.data?.type == 'FriendMessage' &&
       data?.data?.messageChain[1]?.type == 'Plain' &&
-      data?.data?.messageChain[1].text == '你好'
+      data?.data?.messageChain[1].text == config.hello_world.command &&
+      config.hello_world.commandSender.includes(data?.data?.sender?.id)
     ) {
-      sendFriendMessage(data.data.sender.id, [{
-        type: 'Plain',
-        text: 'Hello world'
-      }])
+      sendFriendMessage(data.data.sender.id, [
+        {
+          type: 'Plain',
+          text: config.hello_world.response
+        }
+      ])
     }
   })
 }
